97fc0b378ba2919be7ab2ca24bc71c034fe39954
Marcos Bolanos <marcos.i.bolanos.valerio@intel.com>
2019-11-10 23:38:44 -0600

    Added the part that was labelling the students by missings.

--- Utils/LabelingDatasetUsingMissings.py
+++ Utils/LabelingDatasetUsingMissings.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-def LabelingDatasetUsingCycles(path2csv, OutputFilename, SaveToCsv = True, Verbose=False):
+def LabelingDatasetUsingMissings(path2csv, MissingsLimit, OutputFilename, SaveToCsv = True, Verbose=False):
     df = pd.read_csv(path2csv)
     df.drop(['Unnamed: 0'], axis=1, inplace=True)
-    df.drop(['Materia', 'Repetidor', 'Calificacion', 'Faltas', 'Partial'], axis=1, inplace = True)
-    df_pivot = pd.pivot_table(df, index=['No. Control'], columns=['Cycle'], values=['Faltas'], aggfunc=np.sum).copy()
+    df.drop(['Materia', 'Repetidor', 'Calificacion', 'Semester', 'Partial', 'Group'], axis=1, inplace = True)
+    df_pivot = pd.pivot_table(df, index=['No. Control'], columns=['Cycle'], values=['Faltas'], aggfunc=np.sum, fill_value=-1).copy()
     df_pivot.reset_index(col_level=1, inplace= True)
     df_pivot.columns = df_pivot.columns.droplevel()
     df_pivot['Abandono'] = ''
@@ -14,11 +14,13 @@ def LabelingDatasetUsingCycles(path2csv, OutputFilename, SaveToCsv = True, Verbo
     if Verbose: print("No Rows: " + str(NoRows) + ", No Columns: " + str(NoColumns))
     
     for i in range(NoRows):
-        Abandono = 'Si'
-        '''
-            Needs to be stablish a limit of missings to label the students as 
-            Dropped or Not Dropped
-        '''
+        Abandono = 'No'
+        for j in range(NoColumns-2,0,-1): # Removing 'No. Control' and 'Abandono' columns and checking from last to first cycle
+            if df_pivot.iloc[i][j] == -1:
+                continue
+            if df_pivot.iloc[i][j] >= MissingsLimit:
+                Abandono = 'Si'
+            break
         df_pivot.iloc[i, -1] = Abandono # This is the 'Abandono' Column
 
     if Verbose:
@@ -35,5 +37,5 @@ def LabelingDatasetUsingCycles(path2csv, OutputFilename, SaveToCsv = True, Verbo
 if __name__ == "__main__":                                                      # This main is just to setup some variables before running the script if we 
                                                                                 # run it with "double click" or with python <script name>.py from cmd
     path2csv = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets\FinalDataFrame.csv'
-    OutputFilename = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets\StudentsLabeledByCycle.csv'
-    LabelingDatasetUsingCycles(path2csv, OutputFilename, Verbose=True)
+    OutputFilename = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets\StudentsLabeledByMissings.csv'
+    LabelingDatasetUsingMissings(path2csv, 70, OutputFilename, Verbose=True)
