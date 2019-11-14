3ca237907fbfbfec283c8d21c2f53fa3c9ab73c8
Marcos Bolanos <marcos.ibolanos@gmail.com>
2019-11-14 00:40:17 -0600

    Changing the name of the column 'Ultimo Semestre' by 'Ultimo Ciclo'

--- Utils/MergingTheDataSets.py
+++ Utils/MergingTheDataSets.py
@@ -11,15 +11,15 @@ def MergingTheDataSets(path2MainDF, path2GenderDF, path2LabeledDF, OutputFilenam
     df_main.drop(['Unnamed: 0'], axis=1, inplace=True)
     df_labels.drop(['Unnamed: 0'], axis=1, inplace=True)
     
-    columnsToDrop = set(df_labels.columns) - set(['No. Control', 'Abandono', 'Ultimo Semestre'])
+    columnsToDrop = set(df_labels.columns) - set(['No. Control', 'Abandono', 'Ultimo Ciclo'])
     df_labels.drop(columnsToDrop, axis = 1, inplace=True)
     df_main_l = df_main.merge(df_labels, how='left', on=['No. Control'])
 
     if SaveToCsv:
         tempFilename = OutputFilename.split("\\")
         OutputFolder = OutputFilename.replace(tempFilename[-1], "")
-        if Verbose: print("Saving the merge bewteen main and labels in: {}".format(os.path.join(OutputFolder, 'FinalDataFrame_with_labels.csv')))
-        df_main_l.to_csv(os.path.join(OutputFolder, 'FinalDataFrame_with_labels.csv'))
+        if Verbose: print("Saving the merge bewteen main and labels in: {}".format(os.path.join(OutputFolder, 'FullDataSet_with_labels.csv')))
+        df_main_l.to_csv(os.path.join(OutputFolder, 'FullDataSet_with_labels.csv'))
     
     if Verbose:
         if df_main.shape[0] != df_main_l.shape[0]:
