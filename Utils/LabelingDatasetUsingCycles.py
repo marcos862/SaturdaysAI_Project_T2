42097069244421142073a0dd4aa98a8a6fe965fa
Marcos Bolanos <marcos.ibolanos@gmail.com>
2019-11-13 23:24:15 -0600

    Changing the column 'Ultimo Semestre' by 'Ultimo Ciclo'. This will be more useful.

--- Utils/LabelingDatasetUsingCycles.py
+++ Utils/LabelingDatasetUsingCycles.py
@@ -9,7 +9,7 @@ def LabelingDatasetUsingCycles(path2csv, OutputFilename, SaveToCsv = True, Verbo
     df_pivot.reset_index(col_level=1, inplace= True)
     df_pivot.columns = df_pivot.columns.droplevel()
     df_pivot['Abandono'] = ''
-    df_pivot['Ultimo Semestre'] = ''
+    df_pivot['Ultimo Ciclo'] = ''
     NoRows = df_pivot.shape[0]
     NoColumns = df_pivot.shape[1]
     if Verbose: print("No Rows: " + str(NoRows) + ", No Columns: " + str(NoColumns))
@@ -19,12 +19,12 @@ def LabelingDatasetUsingCycles(path2csv, OutputFilename, SaveToCsv = True, Verbo
         if(not np.isnan(df_pivot.iloc[i][-3])):
             #print("Row = " + str(i) + ", Column = 5")
             Abandono = 'No'
-            LastSemester = df_pivot.iloc[i][-3]
+            LastSemester = df_pivot.columns[-3]
         else:
             for j in range(NoColumns-3, 0, -1): # Removing 'No. Control' and 'Abandono' columns and checking from last to first cycle
                 #print("Row = " + str(i) + ", Column = " + str(j))
                 if not np.isnan(df_pivot.iloc[i][j]):
-                    LastSemester = df_pivot.iloc[i][j]
+                    LastSemester = df_pivot.columns[j]
                     if df_pivot.iloc[i][j] == 6:
                         Abandono = 'No'
                     break
