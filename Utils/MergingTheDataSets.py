c194d5f0bea98e0a3d24e48ccb4feccab1bf5923
Marcos Bolanos <marcos.i.bolanos.valerio@intel.com>
2019-11-18 20:56:59 -0600

    Modifying the script to make it works with the Anonymous Raw Data. Basically, it was removed just the "merging" with the Gender, because this was done previously.

--- Utils/MergingTheDataSets.py
+++ Utils/MergingTheDataSets.py
@@ -1,50 +1,30 @@
-import os
 import pandas as pd
 
-def MergingTheDataSets(path2MainDF, path2GenderDF, path2LabeledDF, OutputFilename, SaveToCsv = True, Verbose=False):
-    df_gender = pd.read_csv(path2GenderDF)
-    df_gender.drop(['Nombre', 'Unnamed: 0'], axis = 1, inplace = True)
-    df_gender = df_gender.drop_duplicates()
-    
+def MergingTheDataSets(path2MainDF, path2LabeledDF, OutputFilename, SaveToCsv = True, Verbose=False):
     df_main = pd.read_csv(path2MainDF)
     df_labels = pd.read_csv(path2LabeledDF)
     df_main.drop(['Unnamed: 0'], axis=1, inplace=True)
     df_labels.drop(['Unnamed: 0'], axis=1, inplace=True)
     
-    columnsToDrop = set(df_labels.columns) - set(['No. Control', 'Abandono', 'Ultimo Ciclo'])
+    columnsToDrop = set(df_labels.columns) - set(['Id Unico', 'Abandono', 'Ultimo Ciclo'])
     df_labels.drop(columnsToDrop, axis = 1, inplace=True)
-    df_main_l = df_main.merge(df_labels, how='left', on=['No. Control'])
+    df_main_l = df_main.merge(df_labels, how='left', on=['Id Unico'])
 
-    if SaveToCsv:
-        tempFilename = OutputFilename.split("\\")
-        OutputFolder = OutputFilename.replace(tempFilename[-1], "")
-        if Verbose: print("Saving the merge bewteen main and labels in: {}".format(os.path.join(OutputFolder, 'FullDataSet_with_labels.csv')))
-        df_main_l.to_csv(os.path.join(OutputFolder, 'FullDataSet_with_labels.csv'), encoding='utf-8-sig')
-    
     if Verbose:
         if df_main.shape[0] != df_main_l.shape[0]:
             print("The merge bewteen df_main and df_labeled was not right, the number of rows before and after are not the same")
         else:
             print("The merge bewteen df_main and df_labeled was successful, the number of rows before and after is the same")
     
-    df_main_lg = pd.merge(df_main_l, df_gender, how='left', on=['No. Control'])
-    
-    if Verbose:
-        if df_main_l.shape[0] != df_main_lg.shape[0]:
-            print("The merge bewteen df_main_labeled and df_gender was not right, the number of rows before and after are not the same")
-        else:
-            print("The merge bewteen df_main_labeled and df_gender was successful, the number of rows before and after is the same")
-            
     if SaveToCsv:
-        if Verbose: print("Saving the dataframe merged with labels and genders in: {}".format(OutputFilename))
-        df_main_lg.to_csv(OutputFilename, encoding='utf-8-sig')
+        if Verbose: print("Saving the dataframe merged with labels in: {}".format(OutputFilename))
+        df_main_l.to_csv(OutputFilename, encoding='utf-8-sig')
         
-    return df_main_lg
+    return df_main_l
 
 if __name__ == "__main__":                                                      # This main is just to setup some variables before running the script if we 
                                                                                 # run it with "double click" or with python <script name>.py from cmd
-    path2MainDF = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets\FullDataSet.csv'
-    path2GenderDF = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets\Gender.csv'
-    path2LabeledDF = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets\StudentsLabeledByCycle.csv'
-    OutputFilename = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets\FullDataSet_with_labels_and_genders.csv'
-    MergingTheDataSets(path2MainDF, path2GenderDF, path2LabeledDF, OutputFilename, Verbose=True)
+    path2MainDF = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\DataSets_Created\FullDataSet.csv'
+    path2LabeledDF = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\DataSets_Created\StudentsLabeledByCycle.csv'
+    OutputFilename = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\DataSets_Created\FullDataSet_with_labels.csv'
+    MergingTheDataSets(path2MainDF, path2LabeledDF, OutputFilename, Verbose=True)
