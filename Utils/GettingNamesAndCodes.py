311d64e5694e2c5b44ba6726fe02ec1c8d309691
Marcos Bolanos <marcos.i.bolanos.valerio@intel.com>
2019-11-18 20:45:19 -0600

    Modify the way the partials were read. To get the names we dont need to check all the Partials, is enough if we only check the 1st partial.

--- Utils/GettingNamesAndCodes.py
+++ Utils/GettingNamesAndCodes.py
@@ -29,33 +29,32 @@ def GettingNamesAndCodes(PathToFiles, OutputFilename, SaveToCsv = True, Verbose=
     for Cycle in Cycles:                                                        # For each cycle folder
         if not os.path.isdir(os.path.join(PathToFiles, Cycle)):                 # if the file is not a directory continue with the following
             continue
-        if Verbose: print("Cycle = {}".format(Cycle))                           # Printing the name of the Current Cycle folder
-        Partials = os.listdir(os.path.join(PathToFiles, Cycle))                 # Listing all folders corresponding to the Partials
-        for Partial in Partials:                                                # for each cycle/partial
-            if not os.path.isdir(os.path.join(PathToFiles, Cycle, Partial)):    # if the file is not a directory continue with the following
+        if Verbose: print("Cycle = {}".format(Cycle))                           # Printing the name of the Current Cycle folder        
+        Partial  = "1"
+        if not os.path.isdir(os.path.join(PathToFiles, Cycle, Partial)):    # if the file is not a directory continue with the following
+            continue
+        if Verbose: print("    Partial = {}".format(Partial))               # Print the name of the current Partial folder
+        Groups = os.listdir(os.path.join(PathToFiles, Cycle, Partial))      # List all the files inside the Cycle/Partial Folder
+        for Group in Groups:                                                # for each cycle/patial/group file
+            if os.path.isdir(os.path.join(PathToFiles, Cycle, Partial, Group)):    # if the file is not a directory continue with the following
                 continue
-            if Verbose: print("    Partial = {}".format(Partial))               # Print the name of the current Partial folder
-            Groups = os.listdir(os.path.join(PathToFiles, Cycle, Partial))      # List all the files inside the Cycle/Partial Folder
-            for Group in Groups:                                                # for each cycle/patial/group file
-                if os.path.isdir(os.path.join(PathToFiles, Cycle, Partial, Group)):    # if the file is not a directory continue with the following
-                    continue
-                if Verbose: print("        Group = {}".format(Group))           # Print the name of the current Group File
-                file = os.path.join(PathToFiles, Cycle, Partial, Group)         # Getting the fullpath and filename to the file
-                df_temp = pd.read_html(file)                                    # Reading the files as HTML, it will produce a list of dataframes
-                df_temp = df_temp[1]                                            # In our case, the Dataframe that is important is the [1]
-                df_temp.dropna(axis = 1, how = 'all', inplace = True)
-                df_temp.columns = df_temp.iloc[0]
-                df_temp.drop(df_temp.index[0], inplace = True)
-                ColumnsToRemove = set(df_temp.columns) - set(['No. Control', 'Nombre'])
-                df_temp.drop(ColumnsToRemove, axis=1, inplace=True)
-                count = 0
-                for i in range(df_temp.shape[0]):
-                    if np.isnan(float(df_temp.iloc[i][0])):
-                        count = i
-                        break
-                df2 = df_temp.iloc[0:count].copy()
-                df2['Nombre'] = df2['Nombre'].apply(lambda x: str(x).replace('(R)', '').replace('(RP)', ''))
-                df = pd.concat([df, df2], ignore_index = True)
+            if Verbose: print("        Group = {}".format(Group))           # Print the name of the current Group File
+            file = os.path.join(PathToFiles, Cycle, Partial, Group)         # Getting the fullpath and filename to the file
+            df_temp = pd.read_html(file)                                    # Reading the files as HTML, it will produce a list of dataframes
+            df_temp = df_temp[1]                                            # In our case, the Dataframe that is important is the [1]
+            df_temp.dropna(axis = 1, how = 'all', inplace = True)
+            df_temp.columns = df_temp.iloc[0]
+            df_temp.drop(df_temp.index[0], inplace = True)
+            ColumnsToRemove = set(df_temp.columns) - set(['No. Control', 'Nombre'])
+            df_temp.drop(ColumnsToRemove, axis=1, inplace=True)
+            count = 0
+            for i in range(df_temp.shape[0]):
+                if np.isnan(float(df_temp.iloc[i][0])):
+                    count = i
+                    break
+            df2 = df_temp.iloc[0:count].copy()
+            df2['Nombre'] = df2['Nombre'].apply(lambda x: str(x).replace('(R)', '').replace('(RP)', ''))
+            df = pd.concat([df, df2], ignore_index = True)
     df.drop_duplicates(inplace = True)
     if SaveToCsv:                                                               # if SaveToCsv is True
         if Verbose: print("Saving the dataframe in: {}".format(OutputFilename))
@@ -65,5 +64,5 @@ def GettingNamesAndCodes(PathToFiles, OutputFilename, SaveToCsv = True, Verbose=
 if __name__ == "__main__":                                                      # This main is just to setup some variables before running the script if we 
                                                                                 # run it with "double click" or with python <script name>.py from cmd
     PathToFiles = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets'
-    OutputFilename = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets\NamesWithoutDuplicates.csv'
+    OutputFilename = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\DataSets_Created\NamesWithoutDuplicates.csv'
     GettingNamesAndCodes(PathToFiles, OutputFilename, Verbose=True)
