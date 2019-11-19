c872bc13b256defd446680070f858be8da9dbe3e
Marcos Bolanos <marcos.i.bolanos.valerio@intel.com>
2019-11-18 20:42:26 -0600

    Modifying the script to read from Anonymous Raw Data that already includes some fixes that previously were done in this script.

--- Utils/JoiningData.py
+++ Utils/JoiningData.py
@@ -9,40 +9,7 @@ This script will be used to join all the data from the datasets.
 import os
 import pandas as pd
 
-def _CorrectingColumnNames(df): # by convention, the _ means this is a private function
-    """
-        There are some columns that have the name
-        duplicated, this is caused because they are
-        taking just the three first letters of the 
-        Subject to determine the name of the column.
-        
-        So, to avoid further issues, we need to remove
-        the names duplicated.
-        
-        We will be doing this addind a consecutive number
-        when the names are duplicated.
-    """
-    ColumnsNameCorrected = []                                       # Starting an empty list for the columns with the names corrected
-    Subject = []                                                    # Starting an empty list for the subjects/topics that are being found
-    NumColumns = len(df.columns)                                    # Getting the number of columns to use it in the for
-    for i in range(NumColumns):                                     # for each column
-        if df.columns[i] == 'Faltas':                               # if the column is called "Faltas"
-            SubjectName = df.columns[i-1]                           # The previous column is the name of the subject
-            if SubjectName in Subject:                              # If this Subject Name is duplicated...
-                counter = 0                                         # We initialize a counter to 0, to replace the name of the column adding a number
-                while (str(SubjectName) + str(counter)) in Subject: # While the SubjectName + Counter is in the Subject List...
-                       counter += 1                                 # we will be increasing the counter
-                SubjectName = str(SubjectName) + str(counter)       # So, the new name will be the SubjectName + the value of the counter    
-            Subject.append(SubjectName)                             # And, we add the name of the subject to the Subject List
-            ColumnsNameCorrected.pop()                              # The previous added was the Name of theSubject, discarding it
-            ColumnsNameCorrected.append(SubjectName)                # Adding the corrected name to the list with the right columns names
-            ColumnsNameCorrected.append('Faltas_' + str(SubjectName))# Adding also, the new name to the column "Faltas", we are adding the subject name
-        else:                                                       # If the column name is not "Faltas"
-            ColumnsNameCorrected.append(str(df.columns[i]))         # We added to the list with the right Columns Name
-    df.columns = ColumnsNameCorrected                               # Replacing the columns name of the dataframe with the ColumnsNameCorrected list
-    return(df, Subject)                                             # Returning the dataframe with columns name modified and the List of subjects found in the dataframe
-
-def CleaningRawDataFrame(df):
+def _CleaningRawDataFrame(df):
     """
         This function will be removing all the columns and
         rows not required.
@@ -50,43 +17,25 @@ def CleaningRawDataFrame(df):
         Also, it's calling a function to remove the issue with the
         columns names duplications.
     """
-    df.columns = df.iloc[0]                                                     # Taking the first row as the name of the columns
-    df.drop(df.index[0], inplace = True)                                        # Removing the first row that we just convert to the columns names
-    columnsToRemove = ['No. Cons', 'PROMEDIO', 'MATERIAS REP', '% REPROBACIÓN', '% APROBACIÓN', '']
-    df.drop(columnsToRemove, axis=1, inplace=True)                              # Removing columns that are not useful (the axis=1 indicate columns, default is rows [axis = 0])
+    if 'TUT' in df.columns:                                                        # If we have a column with the grades from 'TUT' subject
+        df.drop(['TUT', 'Faltas_TUT'], axis=1, inplace=True)                   # Removing the 'TUT' and 'Faltas_TUT' as this columns doesnt have useful information
 
-    df, Subject = _CorrectingColumnNames(df)                                    # Renaming the columns that have the same name
-
-    # The dataframe, as it is, at the end, contain the full name of the subjects
-    # As this information is not useful, at the moment, we want to remove that part of the dataframe
-    # we identify this part of the dataframe because there is an empty row between the data from alumnis
-    # and the real name of the subjects
-    RowNumber = 0                                                               # Starting a counter for the row number
-    for i in range(df.shape[0]):                                                # For each row in the dataframe
-        if df.iloc[i][0] == '':                                                 # Checking if the No Control column in the current row is a 'NaN'
-            RowNumber = i                                                       # If that is the case, we reached the end of the data we care
-            break                                                               # we break the for.
-    # https://www.dataquest.io/blog/settingwithcopywarning/
-    df2 = df.iloc[0:RowNumber].copy()                                           # Doing a copy of the original dataframe with just the data we want
-    if 'TUT' in Subject:                                                        # If we have a column with the grades from 'TUT' subject
-        df2.drop(['TUT', 'Faltas_TUT'], axis=1, inplace=True)                   # Removing the 'TUT' and 'Faltas_TUT' as this columns doesnt have useful information
-        Subject.remove('TUT')                                                   # Also, removing the 'TUT' from the list of subjects
-    
+    Subject = set(df.columns) - set(['Id Unico', 'Genero', 'Repetidor', 'Especialidad', 'Turno'])
+    #Missings = ["Faltas_" + str(c) for c in Subject]
+    Subject = [s for s in Subject if "Faltas_" not in s]
     # There are some subjects that doesnt have a numeric grade, so, we need to
     # change the non numeric grade to numeric grade, we are doing that as follows:
     #   'NP' = 0    # NP means 'No presento' (Exam not taken)
     #   'AC' = 7    # AC means 'Acreditado'  (Exam was passed)
     #   'NA' = 5    # NA means 'No Acreditado' (Exam was not passed)
     for s in Subject:                                                           # Checking all the Subjects in the list
-        df2[s] = df2[s].apply(lambda x: 0 if x=='NP' else 7 if x == 'AC' else 5 if x == 'NA' else -1 if x == '' else x) # Changing all the non-numeric grades to numeric grades
-        df2['Faltas_' + s] = df2['Faltas_' + s].apply(lambda x: -1 if x=='' else x) # Changing all the '' missings to zero
-        df2[s] = df2[s].astype(int)
-        df2['Faltas_' + s] = df2['Faltas_' + s].astype(int)
+        df[s] = df[s].apply(lambda x: 0 if x=='NP' else 7 if x == 'AC' else 5 if x == 'NA' else -1 if x == '' else x) # Changing all the non-numeric grades to numeric grades
+        df['Faltas_' + s] = df['Faltas_' + s].apply(lambda x: -1 if x=='' else x) # Changing all the '' missings to zero
+        df[s] = df[s].astype(int)
+        df['Faltas_' + s] = df['Faltas_' + s].astype(int)
     # Finally, we know that, if an alumni has the string '(R)' in his/her name
     # that means that she/he is coursing again a class or the semester
-    df2['Repetidor'] = df2['Nombre'].apply(lambda x: 'Si' if '(R)' in x else 'No')  # Creating a new column to show if the alumni is coursing again the class/semester
-    df2.drop(['Nombre'], axis=1, inplace=True)                                  # Removing the 'Nombre' Column because is not usefull anymore
-    return df2, Subject
+    return df, Subject
 
 def ReformatingDataFrame(df, Subject):
     """
@@ -123,7 +72,6 @@ def JoinAllFiles(PathToFiles, OutputFilename, SaveToCsv = True, Verbose=False):
         Joining all the files in just one dataframe
     """
     df = pd.DataFrame()                                                         # It create a new empty dataframe
-    
     Cycles = os.listdir(PathToFiles)                                            # Read all the folders corresponding to the Cycle
     for Cycle in Cycles:                                                        # For each cycle folder
         if not os.path.isdir(os.path.join(PathToFiles, Cycle)):                 # if the file is not a directory continue with the following
@@ -140,9 +88,8 @@ def JoinAllFiles(PathToFiles, OutputFilename, SaveToCsv = True, Verbose=False):
                     continue
                 if Verbose: print("        Group = {}".format(Group))           # Print the name of the current Group File
                 file = os.path.join(PathToFiles, Cycle, Partial, Group)         # Getting the fullpath and filename to the file
-                df_temp = pd.read_html(file, keep_default_na=False)             # Reading the files as HTML, it will produce a list of dataframes
-                df_temp = df_temp[1]                                            # In our case, the Dataframe that is important is the [1]
-                df_temp, Subject = CleaningRawDataFrame(df_temp)                # Cleaning the RawDataFrame removing columns not important and removing duplicate name columns
+                df_temp = pd.read_csv(file, keep_default_na=False)             # Reading the files as HTML, it will produce a list of dataframes
+                df_temp, Subject = _CleaningRawDataFrame(df_temp)                # Cleaning the RawDataFrame removing columns not important and removing duplicate name columns
                 df_temp = ReformatingDataFrame(df_temp, Subject)                # Modifying the way the dataframe is presented. Changing Subject Grades and Missings columns to rows
                 df_temp['Cycle'] = Cycle                                        # Adding a new column with the name of the cycle
                 df_temp['Partial'] = Partial                                    # Adding a new column with the name of the Partial
@@ -158,6 +105,6 @@ def JoinAllFiles(PathToFiles, OutputFilename, SaveToCsv = True, Verbose=False):
     
 if __name__ == "__main__":                                                      # This main is just to setup some variables before running the script if we 
                                                                                 # run it with "double click" or with python <script name>.py from cmd
-    PathToFiles = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets'
-    OutputFilename = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets\FullDataSet.csv'
+    PathToFiles = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\Raw_Datasets_Anonymous'
+    OutputFilename = r'C:\SaturdaysAI\SaturdaysAI_Project_T2\DataSets_Created\FullDataSet.csv'
     JoinAllFiles(PathToFiles, OutputFilename, Verbose=True)
